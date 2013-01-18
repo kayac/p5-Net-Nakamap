491c6876c9f73b2844ce50613158a4bd8ef529f7
handlename <nagata@handlena.me>
2013-01-18 11:42:29 +0900

    init

---
+++ lib/Net/Nakamap.pm
@@ -0,0 +1,61 @@
+package Net::Nakamap;
+use 5.008_001;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Nakamap - Perl extention to do something
+
+=head1 VERSION
+
+This document describes Net::Nakamap version 0.01.
+
+=head1 SYNOPSIS
+
+    use Net::Nakamap;
+
+=head1 DESCRIPTION
+
+# TODO
+
+=head1 INTERFACE
+
+=head2 Functions
+
+=head3 C<< hello() >>
+
+# TODO
+
+=head1 DEPENDENCIES
+
+Perl 5.8.1 or later.
+
+=head1 BUGS
+
+All complex software has bugs lurking in it, and this module is no
+exception. If you find a bug please either email me, or add the bug
+to cpan-RT.
+
+=head1 SEE ALSO
+
+L<perl>
+
+=head1 AUTHOR
+
+NAGATA Hiroaki <handlename> E<lt>nagata _at_ handlena _dot_ meE<gt>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2013, NAGATA Hiroaki <handlename>. All rights reserved.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/001_basic.t
@@ -0,0 +1,10 @@
+#!perl -w
+use strict;
+use Test::More;
+
+use Net::Nakamap;
+
+# test Net::Nakamap here
+pass;
+
+done_testing;
